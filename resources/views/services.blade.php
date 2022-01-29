c7ae64a760a70fcd9c37328f1301bb116ffde5f3
JuanParedes01 <juan_1320114117@uptecamac.edu.mx>
2022-01-29 01:10:24 -0600

    Diseñadores

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -64,27 +64,34 @@ https://templatemo.com/tm-550-diagoona
                 <main class="tm-col-right">
                     <section class="tm-content">
                         <div class="media my-3 mb-5 tm-service-media tm-service-media-img-l">
-                            <img src="img/services-1.jpg" alt="Image" class="tm-service-img">
+                            <img src="img/israel1.jpg" alt="Image" class="tm-service-img">
                             <div class="media-body tm-service-text">
-                                <h2 class="mb-4 tm-content-title">Best Services for you</h2>
-                                <p>Our template is simple and effective. This is an alternating simple content with a squared size image. Menu hover is #9CC and text #FFF</p>
+                                <h2 class="mb-4 tm-content-title">Israel Rodriguez Moreno</h2>
+                                <p>Desarrollador encargado del proyecto y director en jefe</p>
                             </div> 
                         </div>
                         <div class="media my-3 mb-5 tm-service-media">                            
                             <div class="media-body tm-service-text">
-                                <h2 class="mb-4 tm-content-title">Customer Satisfaction</h2>
-                                <p>Feel free to contact TemplateMo for any kind of question. If you wish to support us, you can make a PayPal donation. Thank you.</p>
-                            </div> 
-                            <img src="img/services-2.jpg" alt="Image" class="tm-service-img-r">
+                                <h2 class="mb-4 tm-content-title">Monserrat Bello Arismendi</h2>
+                                <p>Encargada del diseño de la pagina y desarrollo de la misma</p>
+                            </div>  
+                            <img src="img/monse1.jpg" alt="Image" class="tm-service-img-r">
                         </div>
                         <div class="media my-3 tm-service-media tm-service-media-img-l">
-                            <img src="img/services-3.jpg" alt="Image" class="tm-service-img">
+                            <img src="img/jpalto.jpg" alt="Image" class="tm-service-img">
                             <div class="media-body tm-service-text">
-                                <h2 class="mb-4 tm-content-title">Template Usage</h2>
-                                <p>You are free to use this template for your website either commercial or personal.</p>
-                                <p>You are NOT allowed to redistribute this template ZIP file for a download purpose on any template collection website.</p>
+                                <h2 class="mb-4 tm-content-title">Juan Pablo Paredes Mendoza</h2>
+                                
+                                <p>Primer encargado de la investigación y apoyo del desarrollo del blog web</p>
                             </div> 
-                        </div>                      
+                        </div>  
+                        <div class="media my-3 mb-5 tm-service-media">                            
+                            <div class="media-body tm-service-text">
+                                <h2 class="mb-4 tm-content-title">Eduardo Viñas Valdez</h2>
+                                <p>Soporte tecnico y segundo encargado de investigación</p>
+                            </div>  
+                            <img src="img/Eduardo3.jpg" alt="Image" class="tm-service-img-r">
+                        </div>                    
                     </section>
                 </main>
             </div>
