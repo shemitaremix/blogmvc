7759b72bfafa3ef6d33ae51445cbf6d0dc26dbe2
JuanParedes01 <juan_1320114117@uptecamac.edu.mx>
2022-01-30 21:49:16 -0600

    Info1

--- resources/views/sidebar-left.blade.php
+++ resources/views/sidebar-left.blade.php
@@ -56,29 +56,31 @@ Licence URI: https://www.os-templates.com/template-terms
     
     <div class="content three_quarter"> 
       <!-- ################################################################################################ -->
-      <h1> - Primer tema</h1>
-      <img class="imgr borderedbox inspace-5" src="../images/demo/imgr.gif" alt="">
-      <p>Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
-      <p>Dapiensociis <a href="#">temper donec auctortortis cumsan</a> et curabitur condis lorem loborttis leo. Ipsumcommodo libero nunc at in velis tincidunt pellentum tincidunt vel lorem.</p>
-      <img class="imgl borderedbox inspace-5" src="../images/demo/imgl.gif" alt="">
-      <p>This is a W3C compliant free website template from <a href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a>. For full terms of use of this template please read our <a href="https://www.os-templates.com/template-terms">website template licence</a>.</p>
-      <p>You can use and modify the template for both personal and commercial use. You must keep all copyright information and credit links in the template and associated files. For more website templates visit our <a href="https://www.os-templates.com/">free website templates</a> section.</p>
-      <p>Portortornec condimenterdum eget consectetuer condis consequam pretium pellus sed mauris enim. Puruselit mauris nulla hendimentesque elit semper nam a sapien urna sempus.</p>
+      <h1> - Sistemas de información</h1>
+      <img class="imgr borderedbox inspace-5" src="../img/sistema.jpg" alt="">
+      <p>En informática, los sistemas de información ayudan a administrar, recolectar, recuperar, procesar, almacenar y distribuir información relevante para los procesos fundamentales y las particularidades de cada organización.</p>
+      <p>Todo sistema de información se compone de una serie de recursos interconectados y en interacción, dispuestos del modo más conveniente en base al propósito informativo trazado, como puede ser recabar información personal, procesar estadísticas, organizar archivos, etc. Estos recursos pueden ser:</p>
+      <p>Recursos humanos. Personal de variada índole y destrezas.
+        Datos. Cualquier tipo de información masiva que precisa de organizarse.
+        Actividades. Procedimientos, pasos a seguir, estaciones de trabajo, etc.
+        Recursos informáticos. Aquellos determinados por la tecnología.
+        </p>
+      
       
       <div id="comments">
-        <h2>Comments</h2>
+        <h2>Commentarios</h2>
         <ul>
           <li>
             <article>
               <header>
                 <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
                 <address>
-                By <a href="#">A Name</a>
+                By <a href="#">Pedro Fernandez</a>
                 </address>
                 <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
               </header>
               <div class="comcont">
-                <p>This is an example of a comment made on a post. You can either edit the comment, delete the comment or reply to the comment. Use this as a place to respond to the post or to share what you are thinking.</p>
+                <p>Que buena información en este blog.</p>
               </div>
             </article>
           </li>
@@ -87,12 +89,12 @@ Licence URI: https://www.os-templates.com/template-terms
               <header>
                 <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
                 <address>
-                By <a href="#">A Name</a>
+                By <a href="#">Scarlett Johansson</a>
                 </address>
                 <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
               </header>
               <div class="comcont">
-                <p>This is an example of a comment made on a post. You can either edit the comment, delete the comment or reply to the comment. Use this as a place to respond to the post or to share what you are thinking.</p>
+                <p>Que buen sitio para aprender y sacar información</p>
               </div>
             </article>
           </li>
@@ -101,12 +103,12 @@ Licence URI: https://www.os-templates.com/template-terms
               <header>
                 <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
                 <address>
-                By <a href="#">A Name</a>
+                By <a href="#">Lana Rhoades</a>
                 </address>
                 <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
               </header>
               <div class="comcont">
-                <p>This is an example of a comment made on a post. You can either edit the comment, delete the comment or reply to the comment. Use this as a place to respond to the post or to share what you are thinking.</p>
+                <p>Me Gusta entrar a esta pagina y aprender más información para la escuela.</p>
               </div>
             </article>
           </li>
@@ -124,14 +126,27 @@ Licence URI: https://www.os-templates.com/template-terms
     
     <div class="content three_quarter"> 
       <!-- ################################################################################################ -->
-      <h1> - segundo tema</h1>
-      <img class="imgr borderedbox inspace-5" src="../images/demo/imgr.gif" alt="">
-      <p>Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
-      <p>Dapiensociis <a href="#">temper donec auctortortis cumsan</a> et curabitur condis lorem loborttis leo. Ipsumcommodo libero nunc at in velis tincidunt pellentum tincidunt vel lorem.</p>
-      <img class="imgl borderedbox inspace-5" src="../images/demo/imgl.gif" alt="">
-      <p>This is a W3C compliant free website template from <a href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a>. For full terms of use of this template please read our <a href="https://www.os-templates.com/template-terms">website template licence</a>.</p>
-      <p>You can use and modify the template for both personal and commercial use. You must keep all copyright information and credit links in the template and associated files. For more website templates visit our <a href="https://www.os-templates.com/">free website templates</a> section.</p>
-      <p>Portortornec condimenterdum eget consectetuer condis consequam pretium pellus sed mauris enim. Puruselit mauris nulla hendimentesque elit semper nam a sapien urna sempus.</p>
+      <h1> - Evolución de los Sistemas de Información</h1>
+      <img class="imgr borderedbox inspace-5" src="../img/Evolución.png" alt="">
+      <p>1950 - 1960: procesamiento de datos electrónicos, sistema de procesamiento de transacciones</p>
+        <p>Durante este período, el papel de SI fue principalmente para realizar actividades como el procesamiento de transacciones, el mantenimiento de registros y la contabilidad. SI se utilizó principalmente para el procesamiento electrónico de datos.
+        </p>
+        <p>1960 a 1970: Sistemas de información gerencial</p>
+         <p> Durante esta era, el rol de SI evolucionó desde TPS hasta Management Information Systems (MSI) o Sistemas de información gerenciales. MSI procesa los datos en informes informativos útiles y proporciona a los gerentes las herramientas para organizar evaluar y administrar de manera eficiente los departamentos dentro de una organización. MSI entrega información en forma de pantallas e informes preespecificados para respaldar la toma de decisiones empresariales. Algunos ejemplos de resultados de MSI son los sistemas de informes de tendencias de costos, análisis de ventas y rendimiento de producción.
+          </p>
+        <p>1970 a 1980: Sistemas de soporte de decisiones</p>
+          <p>En esta era, un avance importante fue la introducción de las computadoras personales (PC). Con la introducción de las PC, se produjo la distribución de la potencia informática o de procesamiento en toda la organización. La función de SI está fuertemente asociada con la administración en lugar de un enfoque técnico en una organización. El papel se centró en el "sistema interactivo basado en computadoras" para ayudar a los responsables de la toma de decisiones a resolver problemas.
+          </p>
+        <p>1980 a 1990: Sistemas de información ejecutivo</p>
+          <p>Este período dio lugar a la informática departamental debido a que muchas organizaciones compran su propio hardware y software para satisfacer sus necesidades departamentales. En lugar de esperar el apoyo indirecto del departamento de servicio corporativo centralizado, los empleados podrían usar sus propios recursos para respaldar sus requisitos laborales. Esta tendencia dio lugar a nuevos desafíos de incompatibilidad de datos, integridad y conectividad en diferentes departamentos. Además, los altos ejecutivos no usaban DSS ni MSI, por lo que se desarrollaron sistemas de información ejecutivo (ESI) o sistemas de soporte ejecutivo (ESS).
+          </p>
+        <p>1990 a 2000: Sistemas de gestión del conocimiento</p>
+         <p> Durante esta era, el rápido crecimiento de las intranets, extranets, internet y otras redes globales interconectadas cambiaron drásticamente las capacidades de SI en los negocios. Se hizo posible circular el conocimiento a diferentes partes del mundo independientemente del tiempo y el espacio.
+          </p>
+          <p>2000 - presente: E-Business</p>
+            <p>Internet y las tecnologías y aplicaciones relacionadas cambiaron la forma en que operan las empresas y las personas trabajan. Las funciones de los sistemas de información en este período siguen siendo las mismas que hace 50 años: mantenimiento de registros, gestión de informes, procesamiento de transacciones, gestión de soporte y procesos de gestión de la organización. Se usa para respaldar el proceso comercial, la toma de decisiones y las ventajas competitiva.
+            </p>
+      
       
       <div id="comments">
         <h2>Comments</h2>
@@ -141,12 +156,12 @@ Licence URI: https://www.os-templates.com/template-terms
               <header>
                 <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
                 <address>
-                By <a href="#">A Name</a>
+                By <a href="#">Julion Alvarez</a>
                 </address>
-                <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
+                <time datetime="2045-04-06T08:15+00:00">Monday, 6<sup>th</sup> January 2045 @08:15:00</time>
               </header>
               <div class="comcont">
-                <p>This is an example of a comment made on a post. You can either edit the comment, delete the comment or reply to the comment. Use this as a place to respond to the post or to share what you are thinking.</p>
+                <p>Me encantó conocer más de la evolución.</p>
               </div>
             </article>
           </li>
@@ -155,12 +170,12 @@ Licence URI: https://www.os-templates.com/template-terms
               <header>
                 <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
                 <address>
-                By <a href="#">A Name</a>
+                By <a href="#">Mia Khalifa</a>
                 </address>
                 <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
               </header>
               <div class="comcont">
-                <p>This is an example of a comment made on a post. You can either edit the comment, delete the comment or reply to the comment. Use this as a place to respond to the post or to share what you are thinking.</p>
+                <p>Gracias a este sitio, logre aprender mucho más y complementar mis trabajos.</p>
               </div>
             </article>
           </li>
@@ -169,12 +184,132 @@ Licence URI: https://www.os-templates.com/template-terms
               <header>
                 <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
                 <address>
-                By <a href="#">A Name</a>
+                By <a href="#">Jhonny Sins</a>
                 </address>
                 <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
               </header>
               <div class="comcont">
-                <p>This is an example of a comment made on a post. You can either edit the comment, delete the comment or reply to the comment. Use this as a place to respond to the post or to share what you are thinking.</p>
+                <p>Excelente pagina.</p>
+              </div>
+            </article>
+          </li>
+        </ul>
+
+        <h1> - Conceptos de cliente y servidor</h1>
+      <img class="imgr borderedbox inspace-5" src="../img/client.jpg" alt="">
+      <p>Importancia del modelo cliente servidor</p>
+      <p>La arquitectura cliente servidor tiene dos partes claramente diferenciadas, por un lado la parte del servidor y por otro la parte de cliente o grupo de clientes donde lo habitual es que un servidor sea una máquina bastante potente con un hardware y software específico que actúa de depósito de datos y funcione como un sistema gestor de base de datos o aplicaciones.</p>
+      <p>Componentes</p>
+      <p>Para entender este modelo vamos a nombrar y definir a continuación algunos conceptos básicos que lo conforman.
+
+        Red: Una red es un conjunto de clientes, servidores y base de datos unidos de una manera física o no física en el que existen protocolos de transmisión de información establecidos.
+        Cliente: El concepto de cliente hace referencia a un demandante de servicios, este cliente puede ser un ordenador como también una aplicación de informática, la cual requiere información proveniente de la red para funcionar.
+        Servidor: Un servidor hace referencia a un proveedor de servicios, este servidor a su vez puede ser un ordenador o una aplicación informática la cual envía información a los demás agentes de la red.
+        Protocolo: Un protocolo es un conjunto de normas o reglas y pasos establecidos de manera clara y concreta sobre el flujo de información en una red estructurada.
+        Servicios: Un servicio es un conjunto de información que busca responder las necesidades de un cliente, donde esta información pueden ser mail, música, mensajes simples entre software, videos, etc.
+        </p>
+      <img class="imgl borderedbox inspace-5" src="../img/Clientserv.png" alt="">
+      
+      
+      <div id="comments">
+        <h2>Comments</h2>
+        <ul>
+          <li>
+            <article>
+              <header>
+                <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
+                <address>
+                By <a href="#">Jose Luis</a>
+                </address>
+                <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
+              </header>
+              <div class="comcont">
+                <p>No se como llegue aqui banda, ayuda.</p>
+              </div>
+            </article>
+          </li>
+          <li>
+            <article>
+              <header>
+                <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
+                <address>
+                By <a href="#">Misha Cross</a>
+                </address>
+                <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
+              </header>
+              <div class="comcont">
+                <p>Que genial es estar en un blog, donde esten más personas conocedoras.</p>
+              </div>
+            </article>
+          </li>
+          <li>
+            <article>
+              <header>
+                <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
+                <address>
+                By <a href="#">Alex Marin</a>
+                </address>
+                <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
+              </header>
+              <div class="comcont">
+                <p>Gracias a su blog, he podido continuar con mi trabajo.</p>
+              </div>
+            </article>
+          </li>
+        </ul>
+
+        <h1> - Computadoras centrales y dedicadas</h1>
+      <img class="imgr borderedbox inspace-5" src="../img/Central.jpg" alt="">
+      <p>Computadora central</p>
+      <p>Una computadora central (en inglés mainframe) es una computadora grande, potente y costosa, usada principalmente por una gran compañía para el procesamiento de una gran cantidad de datos; por ejemplo, para el procesamiento de transacciones bancarias</p>
+      <p>¿Qué es un servidor dedicado?</p>
+      <p>Un servidor dedicado es un equipo informático físico que destina todos sus recursos a proporcionar información y atender las peticiones de otro ordenador (cliente) que ha contratado sus servicios. Por tanto, a diferencia de un servidor compartido, el dedicado trabaja en exclusiva para un solo cliente. Existe un amplio abanico de servidores dedicados entre los que poder elegir para el alojamiento en Internet.</p>
+      <p>Dapiensociis <a href="#">temper donec auctortortis cumsan</a> et curabitur condis lorem loborttis leo. Ipsumcommodo libero nunc at in velis tincidunt pellentum tincidunt vel lorem.</p>
+      
+      
+      
+      <div id="comments">
+        <h2>Comments</h2>
+        <ul>
+          <li>
+            <article>
+              <header>
+                <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
+                <address>
+                By <a href="#">Riley Reid</a>
+                </address>
+                <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
+              </header>
+              <div class="comcont">
+                <p>Gracias a ustedes he salvado la carrera.</p>
+              </div>
+            </article>
+          </li>
+          <li>
+            <article>
+              <header>
+                <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
+                <address>
+                By <a href="#">Kalimba</a>
+                </address>
+                <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
+              </header>
+              <div class="comcont">
+                <p>Me encanto este blog, gracias por su información chicos.</p>
+              </div>
+            </article>
+          </li>
+          <li>
+            <article>
+              <header>
+                <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
+                <address>
+                By <a href="#">Miguel Hidalgo</a>
+                </address>
+                <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
+              </header>
+              <div class="comcont">
+                <p>Este blog tan genial, merece un grito.</p>
               </div>
             </article>
           </li>
