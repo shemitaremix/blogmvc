6d6d601d6ed154388c2198eb4c4843479e40350c
Shemitaremix <israel_1320114066@uptecamac.edu.mx>
2022-01-31 22:29:57 -0600

    boton-enviar

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -75,7 +75,7 @@ https://templatemo.com/tm-550-diagoona
                                 <textarea rows="6" name="Mensaje" class="form-control" placeholder="Mensaje" required=""></textarea>
                             </div>
                             <div class="text-right">
-                                <button type="submit" class="btn btn-big btn-primary">enviar</button>
+                                <button  class="btn btn-big btn-primary">enviar</button>
                             </div>
                         </form>
                     </section>
