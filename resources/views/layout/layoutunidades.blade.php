24bc3ae4d1f75936a2ac27865e39311696e92cdc
unknown <monserrat_1320114183@uptecamac.edu.mx>
2022-03-03 01:21:58 -0600

    monse

--- resources/views/layout/layoutunidades.blade.php
+++ resources/views/layout/layoutunidades.blade.php
@@ -57,12 +57,7 @@ Licence URI: https://www.os-templates.com/template-terms
       <!-- ################################################################################################ -->
       <h1> @yield('tituloprincipal1')</h1>
       <img class="imgr borderedbox inspace-5" src="../img/sistema.jpg" alt="">
-      <p>En informática, los sistemas de información ayudan a administrar, recolectar, recuperar, procesar, almacenar y distribuir información relevante para los procesos fundamentales y las particularidades de cada organización.</p>
-      <p>Todo sistema de información se compone de una serie de recursos interconectados y en interacción, dispuestos del modo más conveniente en base al propósito informativo trazado, como puede ser recabar información personal, procesar estadísticas, organizar archivos, etc. Estos recursos pueden ser:</p>
-      <p>Recursos humanos. Personal de variada índole y destrezas.
-        Datos. Cualquier tipo de información masiva que precisa de organizarse.
-        Actividades. Procedimientos, pasos a seguir, estaciones de trabajo, etc.
-        Recursos informáticos. Aquellos determinados por la tecnología.
+      <p>@yield('contenido1')
         </p>
       
       
@@ -125,25 +120,9 @@ Licence URI: https://www.os-templates.com/template-terms
     
     <div class="content three_quarter"> 
       <!-- ################################################################################################ -->
-      <h1> - Evolución de los Sistemas de Información</h1>
+      <h1> @yield('tituloprincipal2')</</h1>
       <img class="imgr borderedbox inspace-5" src="../img/Evolución.png" alt="">
-      <p>1950 - 1960: procesamiento de datos electrónicos, sistema de procesamiento de transacciones</p>
-        <p>Durante este período, el papel de SI fue principalmente para realizar actividades como el procesamiento de transacciones, el mantenimiento de registros y la contabilidad. SI se utilizó principalmente para el procesamiento electrónico de datos.
-        </p>
-        <p>1960 a 1970: Sistemas de información gerencial</p>
-         <p> Durante esta era, el rol de SI evolucionó desde TPS hasta Management Information Systems (MSI) o Sistemas de información gerenciales. MSI procesa los datos en informes informativos útiles y proporciona a los gerentes las herramientas para organizar evaluar y administrar de manera eficiente los departamentos dentro de una organización. MSI entrega información en forma de pantallas e informes preespecificados para respaldar la toma de decisiones empresariales. Algunos ejemplos de resultados de MSI son los sistemas de informes de tendencias de costos, análisis de ventas y rendimiento de producción.
-          </p>
-        <p>1970 a 1980: Sistemas de soporte de decisiones</p>
-          <p>En esta era, un avance importante fue la introducción de las computadoras personales (PC). Con la introducción de las PC, se produjo la distribución de la potencia informática o de procesamiento en toda la organización. La función de SI está fuertemente asociada con la administración en lugar de un enfoque técnico en una organización. El papel se centró en el "sistema interactivo basado en computadoras" para ayudar a los responsables de la toma de decisiones a resolver problemas.
-          </p>
-        <p>1980 a 1990: Sistemas de información ejecutivo</p>
-          <p>Este período dio lugar a la informática departamental debido a que muchas organizaciones compran su propio hardware y software para satisfacer sus necesidades departamentales. En lugar de esperar el apoyo indirecto del departamento de servicio corporativo centralizado, los empleados podrían usar sus propios recursos para respaldar sus requisitos laborales. Esta tendencia dio lugar a nuevos desafíos de incompatibilidad de datos, integridad y conectividad en diferentes departamentos. Además, los altos ejecutivos no usaban DSS ni MSI, por lo que se desarrollaron sistemas de información ejecutivo (ESI) o sistemas de soporte ejecutivo (ESS).
-          </p>
-        <p>1990 a 2000: Sistemas de gestión del conocimiento</p>
-         <p> Durante esta era, el rápido crecimiento de las intranets, extranets, internet y otras redes globales interconectadas cambiaron drásticamente las capacidades de SI en los negocios. Se hizo posible circular el conocimiento a diferentes partes del mundo independientemente del tiempo y el espacio.
-          </p>
-          <p>2000 - presente: E-Business</p>
-            <p>Internet y las tecnologías y aplicaciones relacionadas cambiaron la forma en que operan las empresas y las personas trabajan. Las funciones de los sistemas de información en este período siguen siendo las mismas que hace 50 años: mantenimiento de registros, gestión de informes, procesamiento de transacciones, gestión de soporte y procesos de gestión de la organización. Se usa para respaldar el proceso comercial, la toma de decisiones y las ventajas competitiva.
+      <p> @yield('contenido2')</
             </p>
       
       
@@ -200,18 +179,9 @@ Licence URI: https://www.os-templates.com/template-terms
 <div class="wrapper row3">
   <main class="hoc container clear"> 
     <div class="content three_quarter"> 
-        <h1> - Conceptos de cliente y servidor</h1>
+        <h1> @yield('tituloprincipal3')</</h1>
       <img class="imgr borderedbox inspace-5" src="../img/client.jpg" alt="">
-      <p>Importancia del modelo cliente servidor</p>
-      <p>La arquitectura cliente servidor tiene dos partes claramente diferenciadas, por un lado la parte del servidor y por otro la parte de cliente o grupo de clientes donde lo habitual es que un servidor sea una máquina bastante potente con un hardware y software específico que actúa de depósito de datos y funcione como un sistema gestor de base de datos o aplicaciones.</p>
-      <p>Componentes</p>
-      <p>Para entender este modelo vamos a nombrar y definir a continuación algunos conceptos básicos que lo conforman.
-
-        Red: Una red es un conjunto de clientes, servidores y base de datos unidos de una manera física o no física en el que existen protocolos de transmisión de información establecidos.
-        Cliente: El concepto de cliente hace referencia a un demandante de servicios, este cliente puede ser un ordenador como también una aplicación de informática, la cual requiere información proveniente de la red para funcionar.
-        Servidor: Un servidor hace referencia a un proveedor de servicios, este servidor a su vez puede ser un ordenador o una aplicación informática la cual envía información a los demás agentes de la red.
-        Protocolo: Un protocolo es un conjunto de normas o reglas y pasos establecidos de manera clara y concreta sobre el flujo de información en una red estructurada.
-        Servicios: Un servicio es un conjunto de información que busca responder las necesidades de un cliente, donde esta información pueden ser mail, música, mensajes simples entre software, videos, etc.
+      <p> @yield('contenido3')</
         </p>
       <img class="imgl borderedbox inspace-5" src="../img/Clientserv.png" alt="">
       
@@ -263,13 +233,9 @@ Licence URI: https://www.os-templates.com/template-terms
           </li>
         </ul>
 
-        <h1> - Computadoras centrales y dedicadas</h1>
+        <h1> @yield('tituloprincipal4')</</h1>
       <img class="imgr borderedbox inspace-5" src="../img/Central.jpg" alt="">
-      <p>Computadora central</p>
-      <p>Una computadora central (en inglés mainframe) es una computadora grande, potente y costosa, usada principalmente por una gran compañía para el procesamiento de una gran cantidad de datos; por ejemplo, para el procesamiento de transacciones bancarias</p>
-      <p>¿Qué es un servidor dedicado?</p>
-      <p>Un servidor dedicado es un equipo informático físico que destina todos sus recursos a proporcionar información y atender las peticiones de otro ordenador (cliente) que ha contratado sus servicios. Por tanto, a diferencia de un servidor compartido, el dedicado trabaja en exclusiva para un solo cliente. Existe un amplio abanico de servidores dedicados entre los que poder elegir para el alojamiento en Internet.</p>
-      <p>Dapiensociis <a href="#">temper donec auctortortis cumsan</a> et curabitur condis lorem loborttis leo. Ipsumcommodo libero nunc at in velis tincidunt pellentum tincidunt vel lorem.</p>
+      <p>@yield('contenido4')</</p>
       
       
       
@@ -332,27 +298,9 @@ Licence URI: https://www.os-templates.com/template-terms
     
     <div class="content three_quarter"> 
       <!-- ################################################################################################ -->
-      <h1> - Arquitectura Cliente-Servidor</h1>
+      <h1> @yield('tituloprincipal5')</</h1>
       <img class="imgr borderedbox inspace-5" src="../images/demo/imgr.gif" alt="">
-      <p>En una arquitectura Cliente-Servidor existe un servidor y múltiples clientes que se conectan al servidor para recuperar todos los recursos necesarios para funcionar, en este sentido, el cliente solo es una capa para representar los datos y se detonan acciones para modificar el estado del servidor, mientras que el servidor es el que hace todo el trabajo pesado.
-        En esta arquitectura, el servidor deberá exponer un mecanismo que permite a los clientes conectarse, que por lo general es TCP/IP, esta comunicación permitirá una comunicación continua y bidireccional, de tal forma que el cliente puede enviar y recibir datos del servidor y viceversa.</p>
-      <p>Los protocolos para la transmisión de datos en internet más importantes son TCP (Protocolo de Control de Transmisión) e IP (Protocolo de Internet). De manera conjunta (TCP/IP) podemos enlazar los dispositivos que acceden a la red, algunos otros protocolos de comunicación asociados a internet son POP, SMTP y HTTP.</p>
-      <img class="imgl borderedbox inspace-5" src="../img/cliente.PNG" alt="">
-      <p>Existen varios tipos de protocolos de red: </p>
-      <p>Protocolos de comunicación de red: protocolos de comunicación de paquetes básicos como TCP / IP y HTTP.</p>
-      <p>Protocolos de seguridad de red: implementan la seguridad en las comunicaciones de red entre servidores, incluye HTTPS, SSL y SFTP.</p>
-      <p>Protocolos de gestión de red: proporcionan mantenimiento y gobierno de red, incluyen SNMP e ICMP.
-        Un grupo de protocolos de red que trabajan juntos en los niveles superior e inferior comúnmente se les denomina familia de protocolos.</p>
-      <p>Muchos sistemas reales comparten importantes propiedades subyacentes y dan lugar a problemas de diseño comunes Se pueden describir modelos o paradigmas de arquitectura software-</p>
-      <ul>
-    
-      <li> Los modelos están pensados para proporcionar una descripción abstracta, simplificada pero consistente de cada aspecto relevante del diseño de un sistema.</li>
-      <li>Un servicio es una parte diferente de un sistema de computadores que gestiona una colección de recursos relacionados y presenta su funcionalidad a los usuarios y aplicaciones.</li>
-      <li>Es similar a un módulo, pero representa un nivel superior, ya que un servicio suele estar compuesto por varios módulos software y puede que también componentes hardware.</li>
-      <li>Así se aplican todos los conceptos de abstracción de datos, específicamente el concepto de interfaz pública.</li>.
-</ul>
-      <p> Un diagrama de componentes proporciona una visión general del sistema y documenta la organización de los componentes del sistema y sus relaciones y dependencias mutuas. Los diagramas de componentes proporcionan una visión orientada a la ejecución, es decir, dan al desarrollador información sobre si un sistema funciona de forma coherente y cumple sus tareas y objetivos.</p>
-      <p> Los objetivos y propósitos más importantes de este tipo de diagrama son el modelado de sistemas de software basados en componentes, la especificación de arquitecturas de software y la división de sistemas en subsistemas (por ejemplo, interfaz gráfica de usuario/IGU, ámbito empresarial y capa de persistencia con base de datos relacional). Asimismo, se asignan tareas y funciones concretas a las subáreas y sus interfaces dentro del sistema.</p>
+      <p>@yield('contenido5')</</p>
       
       <br></br>
       <div id="comments">
